c4ec7571af693f9c0a093c2bb5df21a3a25de8bc
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2021-06-16 10:23:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup
 
 setup(
     name = 'protein-bert',
-    version = '1.0.1',
-    description = 'A BERT-like deep language model for protein sequences.',
-    url = 'https://github.com/nadavbra/protein_bert',
-    author = 'Nadav Brandes',
-    author_email  ='nadav.brandes@mail.huji.ac.il',
     packages = ['proteinbert', 'proteinbert.shared_utils'],
     license = 'MIT',
     scripts = [
