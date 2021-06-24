8476f34a2460e6b6efba90982b21d7334d9cefaa
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-06-24 18:25:21 +0530

    cleanup

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = 'protein-bert',
+    name = 'protein-bert-tfgpu',
     packages = ['proteinbert', 'proteinbert.shared_utils'],
     license = 'MIT',
-    scripts = [
-        'bin/create_uniref_db',
-        'bin/create_uniref_h5_dataset',
-        'bin/pretrain_proteinbert',
-        'bin/set_h5_testset',
-    ],
     install_requires = [
         'tensorflow',
         'tensorflow_addons',
