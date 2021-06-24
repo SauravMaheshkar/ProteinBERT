fc2d257d84873a55cd49c0bd198bb847e281100a
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-06-25 00:06:36 +0530

    Add

--- proteinbert/layers.py
+++ proteinbert/layers.py
@@ -1,12 +1,9 @@
-from typing import Any
-
 import jax.numpy as jnp
+from chex import Array
 from einops import rearrange
+from einops.einops import repeat
 from flax import linen as nn
-
-ATTN_MASK_VALUE = -1e10
-
-Dtype = Any
+from flax.linen.activation import tanh
 
 
 class GlobalLinearSelfAttention(nn.Module):
@@ -16,13 +13,13 @@ class GlobalLinearSelfAttention(nn.Module):
     heads: int
 
     @nn.compact
-    def __call__(self, feats):
+    def __call__(self, feats) -> Array:
 
         inner_dim = self.dim_head * self.heads
         scale = self.dim_head ** -0.5
         h = self.heads
 
-        qkv = nn.Dense(features = 3 * h * self.dim_head, use_bias=False)(feats)
+        qkv = nn.Dense(features=3 * h * self.dim_head, use_bias=False)(feats)
         q, k, v = jnp.split(qkv, 3, axis=-1)
         q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=h), (q, k, v))
 
@@ -33,9 +30,42 @@ class GlobalLinearSelfAttention(nn.Module):
         context = jnp.einsum("b h n d, b h n e -> b h d e", k, v)
         out = jnp.einsum("b h d e, b h n d -> b h n e", context, q)
         out = rearrange(out, "b h n d -> b n (h d)")
-        return nn.Dense(featurers = inner_dim, use_bias = False)(out)
+        return nn.Dense(featurers=inner_dim, use_bias=False)(out)
 
 
 class CrossAttention(nn.Module):
-    def __init__(self) -> None:
-        pass
+
+    dim: int
+    dim_keys: int
+    dim_out: int
+    heads: int
+    dim_head: int = (64,)
+    qk_activation = tanh()
+
+    @nn.compact
+    def __call__(self, x, context) -> Array:
+
+        b, h = x.shape[0], self.heads
+        self.scale = self.dim_head ** -0.5
+        inner_dim = self.dim_head * self.heads
+
+        q = nn.Dense(features=inner_dim, use_bias=False)(x)
+        kv = nn.Dense(features=inner_dim * 2, use_bias=False)(context)
+        k, v = jnp.split(kv, 2, axis=-1)
+
+        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=h), (q, k, v))
+        null_k, null_v = map(
+            lambda t: repeat(t, "d -> b h () d", b=b, h=h),
+            (self.null_key, self.null_value),
+        )
+
+        k = jnp.concatenate((null_k, k), dim=-2)
+        v = jnp.concatenate((null_v, v), dim=-2)
+        q, k = map(lambda t: self.qk_activation(t), (q, k))
+
+        sim = jnp.einsum("b h i d, b h j d -> b h i j", q, k) * self.scale
+        attn = nn.softmax(sim, dim=-1)
+        out = jnp.einsum("b h i j, b h j d -> b h i d", attn, v)
+        out = rearrange(out, "b h n d -> b n (h d)")
+
+        return nn.Dense(features=self.dim_out)(out)
