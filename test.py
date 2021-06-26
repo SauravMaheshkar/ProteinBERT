d8f5cb2df448d6f9e7f1d203b89228291117766f
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-06-26 20:21:53 +0530

    Upload test script

---
+++ test.py
@@ -0,0 +1,21 @@
+from proteinbert import ProteinBERT
+import jax
+from jax import random
+
+
+def test():
+
+    seq = jax.random.randint(
+        key=random.PRNGKey(0), minval=0, maxval=21, shape=(2, 2048)
+    )
+    annotation = jax.random.randint(
+        key=random.PRNGKey(0), minval=0, maxval=1, shape=(2, 8943)
+    )
+
+    init_rngs = {"params": random.PRNGKey(0), "dropout": random.PRNGKey(1)}
+
+    ProteinBERT().init(init_rngs, seq, annotation)
+
+
+if __name__ == "__main__":
+    test()
